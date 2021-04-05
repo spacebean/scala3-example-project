dbf9aeec5b74886196b6569f9e293785960ed678
spacebean <spacebean@outlook.com>
2021-04-05 18:17:19 +0300

    Refactor code.

--- src/main/scala/UnionTypes.scala
+++ src/main/scala/UnionTypes.scala
@@ -1,3 +1,5 @@
+import UnionTypes.List.{Empty, Cons}
+
 /**
  * Union Types: https://dotty.epfl.ch/docs/reference/new-types/union-types.html
  */
@@ -11,11 +13,9 @@ object UnionTypes:
   // You can create type aliases for your union types (sum types).
   type DivisionResult = DivisionByZero | Success
 
-  sealed trait List[+A]
-
-  case object Empty extends List[Nothing]
-
-  final case class Cons[+A](h: A, t: List[A]) extends List[A]
+  enum List[+A]:
+    case Empty extends List[Nothing]
+    case Cons(h: A, t: List[A]) extends List[A]
 
   private def safeDivide(a: Double, b: Double): DivisionResult =
     if b == 0 then DivisionByZero("DivisionByZeroException") else Success(a / b)
